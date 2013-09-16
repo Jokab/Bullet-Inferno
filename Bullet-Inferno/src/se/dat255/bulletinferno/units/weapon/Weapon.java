898faa5c1534717782680ea8169c3206dcab75ed
sebbe33 <seb.blomberg@gmail.com>
2013-09-16 16:15:31 +0200

    Added interface for a Weapon

---
+++ Bullet-Inferno/src/se/dat255/bulletinferno/units/weapon/Weapon.java
@@ -0,0 +1,40 @@
+package se.dat255.bulletinferno.units.weapon;
+
+import com.badlogic.gdx.math.Vector2;
+
+public interface Weapon {
+	/**
+	 * Returns the <strong>constant</strong> reloading time in milliseconds
+	 * @return time in milliseconds
+	 */
+	public int getReloadingTime();
+	
+	/**
+	 * Returns the current time in milliseconds
+	 * until the weapon is ready to fire again
+	 * @return time in milliseconds
+	 */
+	public int getReloadingTimeLeft();
+	
+	/**
+	 * Returns whether the weapon is loaded/ready to fire
+	 * @return loaded
+	 */
+	public boolean isLoaded();
+	
+	/**
+	 * Returns the weapon's offset position
+	 * @return offset
+	 */
+	public Vector2 getOffset();
+	
+	/**
+	 * Fires the weapon from the specified
+	 * origin with the weapon's given offset.
+	 * @param origin
+	 */
+	public void fire(Vector2 origin);
+	
+	
+	
+}
