7ea4bdf07ec0be2d71e2922e26e9a7506d143a45
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-09-16 17:19:08 +0200

    Added javadoc to Enemy interface.

--- Bullet-Inferno/src/se/dat255/bulletinferno/units/enemy/Enemy.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/units/enemy/Enemy.java
@@ -4,17 +4,52 @@ import com.badlogic.gdx.math.Vector2;
 
 public interface Enemy {
 	
+	/**
+	 * Returns the enemy's velocity.
+	 * 
+	 * @return The velocity.
+	 */
 	Vector2 getVelocity();
 	
+	/**
+	 * Sets the enemy's velocity.
+	 * 
+	 * @param velocity The velocity to be set.
+	 */
 	void setVelocity(Vector2 velocity);
 	
+	/**
+	 * Returns the enemy's position.
+	 * 
+	 * @return The position.
+	 */
 	Vector2 getPosition();
 	
+	/**
+	 * Sets the enemy's position.
+	 * 
+	 * @param position The position to be set.
+	 */
 	void setPosition(Vector2 position);
 	
+	/**
+	 * Returns the enemy's hit points.
+	 * 
+	 * @return The hit points.
+	 */
 	int getHitPoints();
 	
+	/**
+	 * Sets the enemy's hit points.
+	 * 
+	 * @param hitpoints The hit points to be set.
+	 */
 	void setHitPoints(int hitpoints);
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 */
+	// TODO: I expect this to be added in a superclass/superinterface.
 	void update(float deltaTime);
 }
