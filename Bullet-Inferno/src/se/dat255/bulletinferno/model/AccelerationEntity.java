802599d7ab13eed4a4b7c4d1fa459e0dc0adff55
Joel Severin <bitbucket@joelhemsida.se>
2013-09-17 19:19:36 +0200

    Made AccelerationEntity a subtype of PositionEntity.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/AccelerationEntity.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/AccelerationEntity.java
@@ -2,7 +2,7 @@ package se.dat255.bulletinferno.model;
 
 import com.badlogic.gdx.math.Vector2;
 
-public interface AccelerationEntity {
+public interface AccelerationEntity extends VelocityEntity {
 	public Vector2 getAcceleration();
 
 	public void setAcceleration(Vector2 acceleration);
