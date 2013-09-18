fc1310ffcba17f5a753963c798efb972d82b54d6
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-09-18 15:01:17 +0200

    Added javadoc to some interfaces.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/PositionEntity.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/PositionEntity.java
@@ -4,6 +4,11 @@ import com.badlogic.gdx.math.Vector2;
 
 public interface PositionEntity {
     
+	/**
+	 * Returns a Vector2 representation of a position of the PositionEntity.
+	 * 
+	 * @return
+	 */
 	public Vector2 getPosition();
 	
 }
--- Bullet-Inferno/src/se/dat255/bulletinferno/model/Projectile.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/Projectile.java
@@ -2,7 +2,7 @@ package se.dat255.bulletinferno.model;
 
 public interface Projectile extends Collidable {
 	/**
-	 * Returns the damage of the projectile
+	 * Returns the damage of the Projectile.
 	 * 
 	 * @return damage
 	 */
