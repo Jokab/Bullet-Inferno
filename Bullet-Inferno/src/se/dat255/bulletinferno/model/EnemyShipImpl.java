ce9b4b9d46b0e2eaed46c3a01eeda7514da8fb3f
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-09-18 10:46:56 +0200

    Filled in overrided functions in EnemyShipImpl.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/EnemyShipImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/EnemyShipImpl.java
@@ -2,60 +2,26 @@ package se.dat255.bulletinferno.model;
 
 import com.badlogic.gdx.math.Vector2;
 
-public class EnemyShipImpl implements EnemyShip {
+public class EnemyShipImpl extends SimpleVelocityEntity implements EnemyShip {
 
-	private int hitPoints;
-	private Vector2 velocity;
-	private Vector2 position;
+	private int health;
+	private final int initialHealth;
+	private int score;
+	private int credits;
 
-	public EnemyShipImpl(Vector2 position, Vector2 velocity, int hitPoints) {
-		this.hitPoints = hitPoints;
-		this.velocity = velocity;
-		this.position = position;
-	}
-
-	public void update(float delta) {
-		position.add(velocity.cpy().scl(delta));
-	}
-
-	@Override
-	public void setVelocity(Vector2 velocity) {
-		this.velocity = velocity;
-	}
-
-	@Override
-	public Vector2 getVelocity() {
-		return velocity;
-	}
-
-	@Override
-	public Vector2 getPosition() {
-		return position;
-	}
-
-	@Override
-	public void setPosition(Vector2 position) {
-		this.position = position;
-	}
-
-	public void setHitPoints(int hitPoints) {
-		this.hitPoints = hitPoints;
-	}
-
-	public int getHitPoints() {
-		return hitPoints;
+	public EnemyShipImpl(Vector2 position, Vector2 velocity, int initialHealth) {
+		super(position, velocity);
+		this.initialHealth = initialHealth;
 	}
 
 	@Override
 	public int getScore() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.score;
 	}
 
 	@Override
 	public int getCredits() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.credits;
 	}
 
 	@Override
@@ -72,19 +38,16 @@ public class EnemyShipImpl implements EnemyShip {
 
 	@Override
 	public void takeDamage(int damage) {
-		// TODO Auto-generated method stub
-
+		this.health -= damage;
 	}
 
 	@Override
 	public int getHealth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.health;
 	}
 
 	@Override
 	public int getInitialHealth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.initialHealth;
 	}
 }
