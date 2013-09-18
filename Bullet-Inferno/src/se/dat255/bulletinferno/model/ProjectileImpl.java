a7f05b2d7da6bca903a93d1083e4fbef44517b7a
sebbe33 <seb.blomberg@gmail.com>
2013-09-18 15:47:39 +0200

    Adapted projectileImpl for an object pool

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/ProjectileImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/ProjectileImpl.java
@@ -3,26 +3,41 @@ package se.dat255.bulletinferno.model;
 import com.badlogic.gdx.math.Vector2;
 
 public class ProjectileImpl implements Projectile {
+	private int damage;
+	private final Game world;
 
-	public ProjectileImpl(Vector2 origin, Vector2 velocity) {
-		
+	/**
+	 * Constructs a new projectile
+	 * @param world
+	 */
+	public ProjectileImpl(Game world) {
+		this.world = world;
 	}
-
+	
+	/**
+	 * Initializes the projectile
+	 * @param origin position
+	 * @param velocity
+	 * @param damage
+	 */
+	public void init(Vector2 origin, Vector2 velocity, int damage) {
+		this.damage = damage;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getDamage() {
-		return 10;
+		return damage;
 	}
 
-	@Override
-	public boolean intersects(Vector2 point) {
-		return false;
-	}
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void collided(Collidable entity) {
-		if(entity instanceof Destructible) {
-			// We know it can be hit
-			((Destructible) entity).takeDamage(this.getDamage());
-		}
+		// Code for special behavior here
+		// TODO Call for removing bullet
 	}
 }
