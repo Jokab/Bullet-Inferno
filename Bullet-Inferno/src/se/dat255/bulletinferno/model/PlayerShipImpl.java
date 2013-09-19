518d67cd13a908bb8f9406f29857eda286404f6e
Anton Hallin <antonhallin3@gmail.com>
2013-09-19 12:56:08 +0200

    Added weapon and game world to PlayerShipImpl

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/PlayerShipImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/PlayerShipImpl.java
@@ -5,13 +5,16 @@ import com.badlogic.gdx.math.Vector2;
 import se.dat255.bulletinferno.MyGame;
 
 public class PlayerShipImpl implements PlayerShip {
-
 	private float y;
 	private float x;
+	private Game world;
+	private Weapon weapon;
 
-	public PlayerShipImpl(final float x, final float y) {
+	public PlayerShipImpl(final float x, final float y, Game world) {
 		this.x = x;
 		this.y = y;
+		this.world = world;
+		weapon = new WeaponImpl(5, world);
 	}
 
 	public float getY() {
@@ -60,6 +63,10 @@ public class PlayerShipImpl implements PlayerShip {
 	public int getInitialHealth() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public Weapon getWeapon() {
+		return weapon;
 	}
 
 }
