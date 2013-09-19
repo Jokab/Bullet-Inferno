8a8e756dceb4cce99d9c7888c26ef70e1f8cd3d9
Joel Severin <bitbucket@joelhemsida.se>
2013-09-19 18:04:01 +0200

    Added basic physics simulation subsystem. No integration.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/PhysicsSimulationImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/PhysicsSimulationImpl.java
@@ -3,11 +3,20 @@ package se.dat255.bulletinferno.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
  * Standard implementation of a physics simulation context and executor.
  */
 public class PhysicsSimulationImpl implements PhysicsSimulation {
-
+	
+    private World world;
+    
+	public PhysicsSimulationImpl() {
+	    
+	}
+	
 	/** A list of elements to run colision detection on. */
 	private List<? extends Collidable> collidables = new ArrayList<Collidable>(0);
 	
@@ -16,26 +25,7 @@ public class PhysicsSimulationImpl implements PhysicsSimulation {
 	 */
 	@Override
 	public void update(float deltaTime) {
-		// Let accelerations affect velocities.
-		/*for(AccelerationEntity entity : accelerationEntities) {
-			Vector2 acceleration = entity.getAcceleration();
-			Vector2 velocity = entity.getVelocity();
-			velocity.add(acceleration.x * deltaTime, acceleration.y * deltaTime);
-			entity.setVelocity(velocity);// (This is also set by .add() above.)
-		}*/
 		
-		// Let velocities affect positions.
-		/*for(VelocityEntity entity : velocityEntities) {
-			Vector2 velocity = entity.getVelocity();
-			Vector2 position = entity.getPosition();
-			position.add(velocity.x * deltaTime, velocity.y * deltaTime);
-			entity.setPosition(position);// (This is also set by .add() above.)
-		}*/
-		
-		// Find collisions and report them.
-		for(Collidable collidable : collidables) {
-			if(collidable != null); // ...
-		}
 	}
 
 	/**
