fd37201280a1f2aa50d97ca6c81f303392d30319
sebbe33 <seb.blomberg@gmail.com>
2013-09-19 12:37:57 +0200

    Added game to weaponImpl

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/WeaponImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/WeaponImpl.java
@@ -5,16 +5,29 @@ import com.badlogic.gdx.math.Vector2;
 public class WeaponImpl implements Weapon {
 	private final Vector2 offset;
 	private final int reloadingTime;
-
+	private final Game world;
+	
 	private int countdown;
 
-	public WeaponImpl(int reloadingTime) {
-		this(reloadingTime, new Vector2());
+	/**
+	 * Constructs a new weapon with the 0-vector
+	 * @param reloadingTime
+	 * @param world
+	 */
+	public WeaponImpl(int reloadingTime, Game world) {
+		this(reloadingTime, world, new Vector2());
 	}
 
-	public WeaponImpl(int reloadingTime, Vector2 offset) {
+	/**
+	 * Constructs a new weapon
+	 * @param reloadingTime
+	 * @param world
+	 * @param offset
+	 */
+	public WeaponImpl(int reloadingTime, Game world, Vector2 offset) {
 		this.offset = offset;
 		this.reloadingTime = reloadingTime;
+		this.world = world;
 	}
 
 	@Override
@@ -40,8 +53,7 @@ public class WeaponImpl implements Weapon {
 	@Override
 	public void fire(Vector2 origin) {
 		if (isLoaded()) {
-			// TODO Add retrieved projectile to world
-			getProjectile(origin.add(getOffset()));
+			//world.addProjectile(getProjectile(origin.add(getOffset())));
 
 			// Start count down
 			countdown = getReloadingTime();
@@ -50,7 +62,9 @@ public class WeaponImpl implements Weapon {
 
 	@Override
 	public Projectile getProjectile(Vector2 origin) {
-		return new ProjectileImpl(origin, new Vector2(1,0));
+		return null;
+		//return new ProjectileImpl(origin, new Vector2(1,0));
+		// TODO Could be replace by Projectile.class and dynamically init
 	}
 
 	// TODO Override gameobject interface
