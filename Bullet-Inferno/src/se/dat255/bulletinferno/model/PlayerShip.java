c98b53ac742be994f75d35bb0968e7b0e54bbf41
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-09-20 13:25:23 +0200

    Added javadoc to PlayerShip.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/PlayerShip.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/PlayerShip.java
@@ -4,7 +4,16 @@ import com.badlogic.gdx.math.Vector2;
 
 public interface PlayerShip extends Ship {
 
+	/**
+	 * Delegates to the PlayerShip's weapon to fire a projectile.
+	 * 
+	 */
 	public void fireWeapon();
 
+	/**
+	 * Sets the PlayerShip's position to the provided position.
+	 * 
+	 * @param position The position to be set.
+	 */
 	void setPosition(Vector2 position);
 }
