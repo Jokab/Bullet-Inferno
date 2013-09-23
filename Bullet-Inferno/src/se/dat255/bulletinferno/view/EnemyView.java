11d9f24911fa35653b91fc3714485561e91d2c10
sebbe33 <seb.blomberg@gmail.com>
2013-09-23 19:48:14 +0200

    Implemented dynamic view in enemyview

--- Bullet-Inferno/src/se/dat255/bulletinferno/view/EnemyView.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/view/EnemyView.java
@@ -1,6 +1,8 @@
 package se.dat255.bulletinferno.view;
 
 import se.dat255.bulletinferno.model.Enemy;
+import se.dat255.bulletinferno.model.Game;
+import se.dat255.bulletinferno.model.Projectile;
 import se.dat255.bulletinferno.model.Renderable;
 
 import com.badlogic.gdx.Gdx;
@@ -11,12 +13,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class EnemyView implements Renderable {
 
-	private final Enemy enemy;
+	private final Game game;
 	private final Texture texture;
 	private final Sprite sprite;
 
-	public EnemyView(Enemy enemy) {
-		this.enemy = enemy;
+	
+	public EnemyView(Game game) {
+		this.game = game;
 
 		texture = new Texture(Gdx.files.internal("data/enemyShip.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
@@ -24,13 +27,15 @@ public class EnemyView implements Renderable {
 		sprite = new Sprite(texture);
 		sprite.setOrigin(0, 0);
 		sprite.setSize(1, 1);
-		sprite.setPosition(enemy.getPosition().x, enemy.getPosition().y);
 	}
 
 	@Override
 	public void render(SpriteBatch batch) {
-		sprite.setPosition(enemy.getPosition().x, enemy.getPosition().y);
-		sprite.draw(batch);
+		for (Enemy enemy : game.getEnemies()) {
+			sprite.setPosition(enemy.getPosition().x,
+					enemy.getPosition().y - sprite.getHeight() / 2);
+			sprite.draw(batch);
+		}
 	}
 
 	@Override
