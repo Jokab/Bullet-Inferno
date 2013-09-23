984f5541a33d6d67329947ede5b0ff6cde8d9d70
Peter Eliasson <warackta@gmail.com>
2013-09-23 18:38:10 +0200

    Fixed tests. * Removed tests for SetPosition as Projectile no longer has a method. * Added init method to the SimpleMockProjectile.

--- Bullet-Inferno-test/src/se/dat255/bulletinferno/model/ProjectileImplTest.java
+++ Bullet-Inferno-test/src/se/dat255/bulletinferno/model/ProjectileImplTest.java
@@ -1,14 +1,11 @@
 package se.dat255.bulletinferno.model;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import se.dat255.bulletinferno.model.mock.SimpleMockGame;
-
-import com.badlogic.gdx.math.Vector2;
 
 public class ProjectileImplTest {
 
@@ -26,33 +23,4 @@ public class ProjectileImplTest {
 		assertTrue("The default damage of a projectile should be above 0",
 				projectile.getDamage() >= 0);
 	}
-
-	@Test
-	public void testSetGetPosition() {
-		// Tests the set and get position methods
-
-		Vector2 position = new Vector2(0, 0);
-		ProjectileImpl projectile = new ProjectileImpl(mockGame);
-		projectile.setPosition(position);
-
-		assertEquals("The setPosition should set the position", position,
-				projectile.getPosition());
-	}
-
-	@Test
-	public void testPositionNotReference() {
-		// Make sure we are not using references to
-		// the Vector2 position vector.
-
-		// Test the setPosition method.
-		Vector2 position = new Vector2(0, 0);
-		ProjectileImpl projectile = new ProjectileImpl(mockGame);
-		projectile.setPosition(position);
-		position.x = 20;
-
-		assertTrue(
-				"Changing the vector outside the object should not affect the object",
-				projectile.getPosition().x != position.x);
-	}
-
 }
