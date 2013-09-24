82adaae7ffa61df92a5766aa000f084d8ff0d5de
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-09-24 14:34:21 +0200

    Removed DefaultEnemyShipImpl implementing EnemyShip. Redundant.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/enemy/DefaultEnemyShipImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/enemy/DefaultEnemyShipImpl.java
@@ -1,11 +1,10 @@
 package se.dat255.bulletinferno.model.enemy;
 
-import se.dat255.bulletinferno.model.EnemyShip;
 import se.dat255.bulletinferno.model.Game;
 
 import com.badlogic.gdx.math.Vector2;
 
-public class DefaultEnemyShipImpl extends EnemyShipImpl implements EnemyShip {
+public class DefaultEnemyShipImpl extends EnemyShipImpl {
 
 	public DefaultEnemyShipImpl(Game game, Vector2 position, Vector2 velocity,
 			int initialHealth) {
