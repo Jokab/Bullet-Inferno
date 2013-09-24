b0845909a093c4ae1d63d2d5fc240c82c37cfda2
Joel Severin <bitbucket@joelhemsida.se>
2013-09-24 14:02:29 +0200

    Formatted ProjectileImplTest.

--- Bullet-Inferno-test/src/se/dat255/bulletinferno/model/ProjectileImplTest.java
+++ Bullet-Inferno-test/src/se/dat255/bulletinferno/model/ProjectileImplTest.java
@@ -26,10 +26,10 @@ public class ProjectileImplTest {
 		// Tests that the projectileImpl has a default damage > 0.
 		ProjectileImpl projectile = new ProjectileImpl(mockGame);
 		projectile.init(new Vector2(), new Vector2(), 30);
-		
+
 		assertTrue("The default damage of a projectile should be above 0",
 				projectile.getDamage() > 0);
-		
+
 		projectile.reset();
 	}
 
@@ -38,7 +38,7 @@ public class ProjectileImplTest {
 		Projectile projectileA = new ProjectileImpl(mockGame);
 		projectileA.init(new Vector2(), new Vector2(), 30);
 		int initialDamage = projectileA.getDamage();
-		
+
 		Projectile projectileB = new ProjectileImpl(mockGame);
 		projectileB.init(new Vector2(), new Vector2(), 30);
 
@@ -49,7 +49,7 @@ public class ProjectileImplTest {
 		projectileA.postCollided(projectileB);
 		assertTrue("A projectile should not take damage upon hit by another projectile (post)",
 				projectileA.getDamage() == initialDamage);
-		
+
 		projectileA.reset();
 		projectileB.reset();
 	}
@@ -59,7 +59,7 @@ public class ProjectileImplTest {
 		Projectile projectile = new ProjectileImpl(mockGame);
 		projectile.init(new Vector2(), new Vector2(), 30);
 		int initialDamage = projectile.getDamage();
-		
+
 		PlayerShip ship = new PlayerShipImpl(mockGame, new Vector2(), 10, WeaponData.STANDARD);
 
 		// If your change fails this test: think again! The order of collision pairs is not defined!
@@ -70,20 +70,20 @@ public class ProjectileImplTest {
 		projectile.postCollided(ship);
 		assertTrue("A projectile should get damage 0 when hitting a ship in postCollided",
 				projectile.getDamage() == 0);
-		
+
 		// Called when hit: projectile.reset();
 	}
-	
+
 	@Test
 	public void testPhysicsBodyAddedCollidedRemoved() {
 		Projectile projectile = new ProjectileImpl(mockGame);
 		projectile.init(new Vector2(), new Vector2(), 30);
-		
+
 		PlayerShip ship = new PlayerShipImpl(mockGame, new Vector2(), 10, WeaponData.STANDARD);
-		
+
 		PhysicsBody body = null;
-		for(CreateBodyCall call : mockGame.physicsWorld.createBodyCalls) {
-			if(call.collidable == projectile) {
+		for (CreateBodyCall call : mockGame.physicsWorld.createBodyCalls) {
+			if (call.collidable == projectile) {
 				body = call.returnValue;
 				break;
 			}
@@ -91,27 +91,27 @@ public class ProjectileImplTest {
 		assertTrue("Adds a physics body to the physics world upon construction", body != null);
 
 		boolean projectilePresentPreCollision = false;
-		for(RemoveBodyCall call : mockGame.physicsWorld.removeBodyCalls) {
-			if(call.body == body) {
+		for (RemoveBodyCall call : mockGame.physicsWorld.removeBodyCalls) {
+			if (call.body == body) {
 				projectilePresentPreCollision = true;
 				break;
 			}
 		}
 		assertTrue("Does not remove the body from the physics world immediately",
 				!projectilePresentPreCollision);
-		
+
 		projectile.preCollided(ship);
 		projectile.postCollided(ship);
 		boolean projectileRemovedPostCollision = false;
-		for(RemoveBodyCall call : mockGame.physicsWorld.removeBodyCalls) {
-			if(call.body == body) {
+		for (RemoveBodyCall call : mockGame.physicsWorld.removeBodyCalls) {
+			if (call.body == body) {
 				projectileRemovedPostCollision = true;
 				break;
 			}
 		}
 		assertTrue("Does remove the physics body of a projectile from the world upon ship hits",
 				projectileRemovedPostCollision);
-		
+
 		// Called when hit: projectile.reset();
 	}
 
