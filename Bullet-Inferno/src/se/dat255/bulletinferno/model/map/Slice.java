602d37b0b0f9f5e9caddec509d95ca09a2799fdf
sebbe33 <seb.blomberg@gmail.com>
2013-09-27 12:04:38 +0200

    Defined methods in the slice interface

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/map/Slice.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/map/Slice.java
@@ -1,5 +1,36 @@
 package se.dat255.bulletinferno.model.map;
 
-public interface Slice {
+import java.util.List;
 
+import se.dat255.bulletinferno.model.PhysicsBody;
+import se.dat255.bulletinferno.util.Disposable;
+
+/**
+ * An interface to define the properties of a slice
+ * in a Segment. 
+ *
+ */
+public interface Slice extends Disposable {
+	/**
+	 * Returns the height, measured in meters
+	 * above sea-level, for the entry point of the slide
+	 * @return height in meters
+	 */
+	public int getEntryHight();
+	
+	/**
+	 * Returns the height, measured in meters
+	 * above sea-level, for the exit point of the slide
+	 * @return height in meters
+	 */
+	public int getExitHeight();
+	
+	/**
+	 * Returns the physics bodies contained
+	 * in this slice. I.e. all the physical space
+	 * of this slice
+	 * @return physics bodies
+	 */
+	public List<PhysicsBody> getPhysicsBodyies();
+	
 }
