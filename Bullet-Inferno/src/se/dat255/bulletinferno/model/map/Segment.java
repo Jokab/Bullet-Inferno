09f1cc269192245a7d1eb64c4ffcf3ac777fb82f
Marc Jamot <marcjamot@gmail.com>
2013-09-27 12:08:42 +0200

    Updated Segment.java with methods and Java doc

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/map/Segment.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/map/Segment.java
@@ -1,5 +1,23 @@
 package se.dat255.bulletinferno.model.map;
 
-public interface Segment {
+import se.dat255.bulletinferno.util.Disposable;
 
+/**
+ * A segment is a holder of map slices, building up a part that
+ *  looks alike in the game.
+ */
+public interface Segment extends Disposable {
+	/** Gets all slices contained within this segment */
+	public Slice[] getSlices();
+	/** Gets the width of all the slices */
+	public int getWidth();
+	/** Gets the start X position of the Segment */
+	public float getPosition();
+	/** Sets the start X position of the Segment
+	 *   and all the slices within */
+	public void setPosition();
+	/** Gets the entry height of the segment */
+	public int getEntryHeight();
+	/** Gets the exit height of the segment */
+	public int getExitHeight();
 }
