1738f41e9fac9b2e536f76e9e84e2febc125cd30
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-09-30 19:16:35 +0200

    Added ResourceIdentifier interface which all classes that want a resource associated with it need to implement.

---
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/ResourceIdentifier.java
@@ -0,0 +1,12 @@
+package se.dat255.bulletinferno.model;
+
+public interface ResourceIdentifier {
+
+	/**
+	 * Returns the identifier that this {@link ResourceIdentifier} uses to retrieve
+	 * its connected resource.
+	 * 
+	 * @return
+	 */
+	String getIdentifier();
+}
