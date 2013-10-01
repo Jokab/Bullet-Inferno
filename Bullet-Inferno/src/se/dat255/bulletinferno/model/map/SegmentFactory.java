a28dd446ced08288411810292f6ca16ca0d9f2c7
sebbe33 <seb.blomberg@gmail.com>
2013-10-01 20:54:01 +0200

    Added link between Segment and slice to SegmentFactory

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/map/SegmentFactory.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/map/SegmentFactory.java
@@ -3,7 +3,25 @@ package se.dat255.bulletinferno.model.map;
 import java.util.Random;
 
 public enum SegmentFactory {
-	WATER, EARTH, TUNNEL;
+	WATER(SliceType.WATER, SliceType.WATER, new SliceType[] {SliceType.WATER}), 
+	MOUNTAIN(SliceType.MOUNTAIN_1, SliceType.MOUNTAIN_8, 
+			new SliceType[] {SliceType.MOUNTAIN_8,
+				SliceType.MOUNTAIN_2,
+				SliceType.MOUNTAIN_3,
+				SliceType.MOUNTAIN_4,
+				SliceType.MOUNTAIN_5,
+				SliceType.MOUNTAIN_6,
+				SliceType.MOUNTAIN_7});	
+	
+	private final SliceType entrySlice;
+	private final SliceType exitSlice;
+	private final SliceType[] suitableSlices;
+	
+	SegmentFactory(SliceType entry, SliceType exit, SliceType[] suitableSlices) {
+		this.entrySlice = entry;
+		this.exitSlice = exit;
+		this.suitableSlices = suitableSlices;
+	}
 	
 	public Segment generateRandomSegment() {
 		SegmentFactory[] values = SegmentFactory.values();
