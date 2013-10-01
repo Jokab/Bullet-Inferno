6be324710f789b2857cd0c8682da4f1505cdb5aa
sebbe33 <seb.blomberg@gmail.com>
2013-10-01 13:59:11 +0200

    Changed bodies parameter in construct to be body definitions

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/map/SliceImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/map/SliceImpl.java
@@ -1,21 +1,40 @@
 package se.dat255.bulletinferno.model.map;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import se.dat255.bulletinferno.model.Collidable;
 import se.dat255.bulletinferno.model.Game;
 import se.dat255.bulletinferno.model.PhysicsBody;
+import se.dat255.bulletinferno.model.PhysicsBodyDefinition;
 import se.dat255.bulletinferno.model.PhysicsWorld;
 
-public class SliceImpl implements Slice {
+public class SliceImpl implements Slice, Collidable {
 	private final int entryHeight;
 	private final int exitHeight;
 	private final Game game;
-	private final List<PhysicsBody> bodies;
+	private final List<PhysicsBody> bodies = new LinkedList<PhysicsBody>();
 	
-	public SliceImpl(Game game, int entryHeight, int exitHeight, List<PhysicsBody> bodies) {
+	public SliceImpl(Game game, int entryHeight, int exitHeight, int x, 
+			List<PhysicsBodyDefinition> bodyDefinitions) {
 		this.entryHeight = entryHeight;
 		this.exitHeight = exitHeight;
-		this.bodies = bodies;
+		this.game = game;
+		
+		// Define bodies
+		for(PhysicsBodyDefinition def : bodyDefinitions) {
+			// Create a new body from the definition,
+			// add the slice's world coordinates to the definitions
+			// position relative to the slice, to get world coordinate
+			game.getPhysicsWorld().createBody(def, this, 
+					def.getBox2DBodyDefinition().position.add(x, 0));
+		}
+		
+	}
+	
+	public SliceImpl(Game game, int entryHeight, int exitHeight, int x) {
+		this.entryHeight = entryHeight;
+		this.exitHeight = exitHeight;
 		this.game = game;
 	}
 	
@@ -25,9 +44,11 @@ public class SliceImpl implements Slice {
 	@Override
 	public void dispose() {
 		PhysicsWorld world = game.getPhysicsWorld();
-		// Remove all bodies from world
-		for(PhysicsBody body : bodies) {
-			world.removeBody(body);
+		// Remove all bodies from world, if there is any
+		if(bodies != null) {
+			for(PhysicsBody body : bodies) {
+				world.removeBody(body);
+			}
 		}
 	}
 
@@ -53,6 +74,22 @@ public class SliceImpl implements Slice {
 	@Override
 	public List<PhysicsBody> getPhysicsBodyies() {
 		return bodies;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void preCollided(Collidable other) {
+		// NOP
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void postCollided(Collidable other) {
+		// NOP
 	}
 	
 }
