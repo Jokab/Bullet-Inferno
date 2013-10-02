b7f2fd1a9974867170563d140f771655023d5cd6
Peter Eliasson <warackta@gmail.com>
2013-10-02 11:01:23 +0200

    Added a test for runLater method of Game to GameImplTest.

--- Bullet-Inferno-test/src/se/dat255/bulletinferno/model/GameImplTest.java
+++ Bullet-Inferno-test/src/se/dat255/bulletinferno/model/GameImplTest.java
@@ -1,5 +1,6 @@
 package se.dat255.bulletinferno.model;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -7,13 +8,23 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import se.dat255.bulletinferno.model.mock.SimpleMockProjectile;
+import se.dat255.bulletinferno.model.mock.SimplePlayerShipMock;
 import se.dat255.bulletinferno.test.Common;
 
 public class GameImplTest {
-	
+
+	class RunnableSpy implements Runnable {
+		public boolean wasRun = false;
+
+		@Override
+		public void run() {
+			wasRun = true;
+		}
+	}
+
 	@BeforeClass
 	public static void beforeTests() {
-		Common.loadEssentials();	
+		Common.loadEssentials();
 	}
 
 	@Test
@@ -55,6 +66,25 @@ public class GameImplTest {
 		GameImpl game = new GameImpl(null);
 		assertNotNull("A timer should be returned from the getTimer method",
 				game.getTimer());
+	}
+
+	@Test
+	public void testRunLater() {
+		// Tests that runLater tasks are run the next update tick.
+		
+		
+		GameImpl game = new GameImpl();
+		game.setPlayerShip(new SimplePlayerShipMock());
+
+		RunnableSpy task = new RunnableSpy();
+
+		game.runLater(task);
+
+		assertFalse("The task should not be run when it is first added.", task.wasRun);
+
+		game.update(1);
+
+		assertTrue("The task should have been run after a call to game.update", task.wasRun);
 	}
 
 }
