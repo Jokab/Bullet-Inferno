e93bb9e108a2c8878373028e6402644361561c35
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-10-02 10:38:36 +0200

    Wrote more javadoc for PlayerShip.

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/PlayerShip.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/PlayerShip.java
@@ -1,6 +1,5 @@
 package se.dat255.bulletinferno.model;
 
-
 import com.badlogic.gdx.math.Vector2;
 
 public interface PlayerShip extends Ship, ResourceIdentifier {
@@ -11,6 +10,7 @@ public interface PlayerShip extends Ship, ResourceIdentifier {
 	 */
 	public void fireWeapon();
 
+	// TODO: javadoc this in some good way
 	public void update(float deltaTime);
 
 	/**
@@ -21,17 +21,55 @@ public interface PlayerShip extends Ship, ResourceIdentifier {
 	 */
 	void setPosition(Vector2 position);
 
+	/**
+	 * Makes the PlayerShip move to the specified position.
+	 * 
+	 * @param yPos
+	 *        The position that the ship should move to.
+	 */
 	public void moveTo(float yPos);
 
+	/**
+	 * Stops the PlayerShip's movement.
+	 */
 	public void stopMovement();
 
+	/**
+	 * Returns the position that the PlayerShip is currently moving to.
+	 * 
+	 * @return The position.
+	 */
 	public float getMovePos();
 
+	/**
+	 * Returns the Weapon that is currently in the PlayerShip's Loadout.
+	 * 
+	 * @return The Weapon.
+	 */
 	public Weapon getWeapon();
 
+	/**
+	 * Returns the Loadout that the PlayerShip is currently equipped with.
+	 * 
+	 * @return The Loadout
+	 */
 	public Loadout getLoadout();
-	
+
+	/**
+	 * Attaches a PassiveAbility to the PlayerShip. This ability is constantly active, and in
+	 * essence
+	 * it modifies some attribute (field) of the PlayerShip, or an attribute of one of its fields.
+	 * 
+	 * @param passiveAbility
+	 *        The PassiveAbility to be set.
+	 */
 	public void attachPassive(PassiveAbility passiveAbility);
 
+	/**
+	 * Sets the modifier for taking damage to the specified value.
+	 * 
+	 * @param takeDamageModifier
+	 *        The modifier value.
+	 */
 	void setTakeDamageModifier(float takeDamageModifier);
 }
