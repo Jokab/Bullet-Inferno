eb440514f711db6cb57b075db59e14be9b9a7875
Simon Österberg <blu3dx@gmail.com>
2013-10-04 20:13:01 +0200

    Fixed tests

--- Bullet-Inferno-test/src/se/dat255/bulletinferno/model/EnemyTypesTest.java
+++ Bullet-Inferno-test/src/se/dat255/bulletinferno/model/EnemyTypesTest.java
@@ -30,7 +30,7 @@ public class EnemyTypesTest {
 
 	@Test
 	public void testEnemyHealth() {
-		SimpleEnemy enemy = EnemyType.DEFAULT_ENEMY_SHIP.getEnemyShip(mockGame, new Vector2());
+		SimpleEnemy enemy = EnemyType.DEFAULT_ENEMY_SHIP.getEnemyShip(mockGame, new Vector2(), false);
 
 		assertTrue(EnemyType.DEFAULT_ENEMY_SHIP.getInitialHealth() == enemy.getHealth());
 	}
@@ -38,7 +38,7 @@ public class EnemyTypesTest {
 	@Test
 	public void testEnemyPosition() {
 		Vector2 position = new Vector2(1, 1);
-		SimpleEnemy enemy = EnemyType.SPECIAL_ENEMY_SHIP.getEnemyShip(mockGame, position);
+		SimpleEnemy enemy = EnemyType.SPECIAL_ENEMY_SHIP.getEnemyShip(mockGame, position, false);
 
 		assertEquals(
 				"Position that was sent to the factory should be the same as the created enemy's position. ",
