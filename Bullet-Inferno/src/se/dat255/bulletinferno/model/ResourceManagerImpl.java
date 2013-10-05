4ab2abf83a2fbb2c974018ad4bc60169fbf3773f
Anton <antonhallin3@gmail.com>
2013-10-06 01:23:14 +0200

    Added new texture to ResourceManager

--- Bullet-Inferno/src/se/dat255/bulletinferno/model/ResourceManagerImpl.java
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/ResourceManagerImpl.java
@@ -26,7 +26,7 @@ public class ResourceManagerImpl implements ResourceManager {
 		// Enemies
 		map.put("DEFAULT_ENEMY_SHIP", "data/defaultEnemy.png");
 		map.put("SPECIAL_ENEMY_SHIP", "data/specialEnemy.png");
-		map.put("BOSS_ENEMY_SHIP", "data/boss.png");
+		map.put("BOSS_ENEMY_SHIP", "data/bossEnemy.png");
 		
 		// Player ship
 		map.put("PLAYER_DEFAULT", "data/playerShip.png");
