5c2ab64f7badcc929508e4585e5d2124e0522b21
Anton <antonhallin3@gmail.com>
2013-10-06 01:33:38 +0200

    Renamed boss-class

---
+++ Bullet-Inferno/src/se/dat255/bulletinferno/model/enemy/DefaultBossImpl.java
@@ -0,0 +1,99 @@
+package se.dat255.bulletinferno.model.enemy;
+
+import se.dat255.bulletinferno.model.Game;
+import se.dat255.bulletinferno.model.PlayerShip;
+import se.dat255.bulletinferno.model.Ship;
+import se.dat255.bulletinferno.model.Weapon;
+import se.dat255.bulletinferno.model.physics.PhysicsBodyDefinition;
+import se.dat255.bulletinferno.model.physics.PhysicsMovementPattern;
+import se.dat255.bulletinferno.util.Timer;
+import se.dat255.bulletinferno.util.Timerable;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class DefaultBossImpl extends SimpleBoss implements Ship, Timerable {
+
+	private Weapon[] weapons;
+	private Timer[] timers;
+	private final PlayerShip player;
+
+	/**
+	 * Constructs a new Angry Boss
+	 * 
+	 * @param game
+	 *        The game instance
+	 * @param type
+	 *        The enemy definition
+	 * @param position
+	 * @param velocity
+	 * @param pattern
+	 *        The movement pattern
+	 * @param initialHealth
+	 * @param weapons
+	 * @param score
+	 *        The score rewarded when boss is killed
+	 * @param credits
+	 *        The credit rewarded when boss is killed
+	 * @param offsets
+	 */
+	public DefaultBossImpl(Game game, EnemyType type, Vector2 position, Vector2 velocity,
+			PhysicsMovementPattern pattern, int initialHealth, Weapon[] weapons, int score,
+			int credits, PhysicsBodyDefinition bodyDefinition) {
+		super(game, type, position, velocity, initialHealth, weapons, score, credits,
+				bodyDefinition, pattern);
+
+		this.player = game.getPlayerShip();
+		this.weapons = weapons;
+		this.timers = new Timer[weapons.length];
+		for (int i = 0; i < weapons.length; i++) {
+			timers[i] = weapons[i].getTimer();
+			timers[i].registerListener(this);
+			timers[i].stop();
+		}
+
+	}
+
+	@Override
+	public void onTimeout(Timer source, float timeSinceLast) {
+
+		fireSpread();
+
+		if (getHealth() < getInitialHealth() / 2) {
+			fireAimed();
+		}
+
+	}
+
+	public void fireSpread() {
+
+		for (int i = (int) weapons.length / 2; i < weapons.length; i++) {
+			weapons[i].fire(this.getPosition(), new Vector2(-1, 0), this);
+		}
+
+	}
+
+	public void fireAimed() {
+		for (int i = 0; i < (int) weapons.length / 2; i++) {
+
+			weapons[i].fire(this.getPosition(), new Vector2(player.getPosition().x
+					- getPosition().x, player.getPosition().y - getPosition().y).nor(), this);
+
+		}
+
+	}
+
+	@Override
+	public void viewportIntersectionBegin() {
+		super.viewportIntersectionBegin();
+
+		for (int i = 0; i < weapons.length; i++) {
+			timers[i].start();
+		}
+
+	}
+
+	@Override
+	public Vector2 getDimensions() {
+		return new Vector2(1, 1);
+	}
+}
