e8ea3ebb93dff3629a0d75c8797a0fcf795ee1d9
Marc Jamot <marcjamot@gmail.com>
2013-10-08 11:52:11 +0200

    Initial ScoreView.java

---
+++ Bullet-Inferno/src/se/dat255/bulletinferno/view/gui/ScoreView.java
@@ -0,0 +1,66 @@
+package se.dat255.bulletinferno.view.gui;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import se.dat255.bulletinferno.util.ManagedTexture;
+import se.dat255.bulletinferno.util.ResourceManager;
+import se.dat255.bulletinferno.util.ResourceManagerImpl.TextureType;
+import se.dat255.bulletinferno.view.Renderable;
+
+public class ScoreView implements Renderable {
+	
+	/** Reference to the resourcemanager */
+	private final ResourceManager resourceManager;
+	
+	/** The texture that holds the numbers */
+	private final ManagedTexture numberImage;
+	/** The regions for the different numbers */
+	private final TextureRegion[] numberRegions;
+	/** The backing array of the score */
+	private final int[] score = new int[10];
+	
+	/**
+	 * Loads the initial images of the score view
+	 * @param resourceManager The manager that holds the assets
+	 */
+	public ScoreView(ResourceManager resourceManager) {
+		this.resourceManager = resourceManager;
+		numberImage = resourceManager.getManagedTexture(TextureType.HUD_NUMBERS);
+		Texture t = numberImage.getTexture();
+		numberRegions = new TextureRegion[]{
+			new TextureRegion(t, 19, 20, 31, 68),
+			new TextureRegion(t, 64, 20, 17, 68),
+			new TextureRegion(t, 97, 20, 31, 68),
+			new TextureRegion(t, 144, 20, 33, 68),
+			new TextureRegion(t, 190, 20, 33, 68),
+			new TextureRegion(t, 19, 111, 31, 68),
+			new TextureRegion(t, 64, 111, 33, 69),
+			new TextureRegion(t, 112, 111, 33, 69),
+			new TextureRegion(t, 157, 111, 32, 69),
+			new TextureRegion(t, 207, 111, 31, 69),
+		};
+	}
+	
+	/** Sets the backing array of the graphic score */
+	public void setScore(int score){
+		for(int i = 9; i >= 0; i--){
+			this.score[i] = score % 10;
+			score /= 10;
+		}
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		// First run i until not 0, unless last number, then 0 is ok
+		for(int i = 0; i < 10; i++){
+			batch.draw(numberRegions[score[i]], i, 1, 1, 1);
+		}
+	}
+
+	@Override
+	public void dispose() {
+		numberImage.dispose(resourceManager);
+	}
+}
